82f3de0398485506a9d6d3490956c4e7893ef377
Jose Joaquim <franca.josejunior@outlook.com>
2022-10-08 17:26:17 -0300

    running on ...

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-var express=require('express');
-var app=express();
-
-var routes=require('./routes/route.js');
-
-app.set('view engine','ejs');
-
-app.use(express.static(__dirname + '/public'));
-
-app.get('/',routes.home);
-
+var express = require("express");
+var app = express();
 var port = process.env.PORT || 3000;
+var os = require("os");
 
-var server=app.listen(port,function(req,res){
-    console.log("Catch the action at http://localhost:"+port);
+app.get("/", (req, res) => {
+  var osname = os.hostname();
+  var message = `Running on ${osname}`;
+  res.send(message);
+  console.info(message);
+});
+
+var server = app.listen(port, function (req, res) {
+  console.log("Catch the action at http://localhost:" + port);
 });
